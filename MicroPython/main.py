13f0534903ae1f8fa1ba0ab227b4540c3b87b389
Patrick Coxall <mr.coxall@mths.ca>
2023-08-31 18:10:23 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,23 @@
 """
 Created by: Mr. Coxall
 Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+This module calculates area and perimeter of rectangle.
 """
 
 from microbit import *
 
+# variables
+area = 3 * 5
+perimeter = 2 * (3 + 5)
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.is_pressed():
+        display.scroll("The dimensions of a rectangle are 5 cm and 3 cm.")
+        display.scroll("Area is " + str(area) + " cm^2.")
+        display.scroll("Perimeter is " + str(perimeter) + " cm.")
+
+        display.clear()
+        display.show(Image.HAPPY)
