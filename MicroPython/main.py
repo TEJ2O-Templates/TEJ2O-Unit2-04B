67b1ca5d17fef620755870b80b5b8aaf727c1073
Patrick Coxall <mr.coxall@mths.ca>
2023-08-31 18:48:06 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,23 +1,21 @@
 """
 Created by: Mr. Coxall
 Created on: Sep 2020
-This module calculates area and perimeter of rectangle.
+This module shows current temperature.
 """
 
 from microbit import *
 
 # variables
-area = 3 * 5
-perimeter = 2 * (3 + 5)
+current_temperature = 0
 
 display.clear()
 display.show(Image.HAPPY)
 
 while True:
     if button_a.is_pressed():
-        display.scroll("The dimensions of a rectangle are 5 cm and 3 cm.")
-        display.scroll("Area is " + str(area) + " cm^2.")
-        display.scroll("Perimeter is " + str(perimeter) + " cm.")
+        current_temperature = temperature()
+        display.scroll("The temperature is " + str(current_temperature) + " C.")
 
         display.clear()
         display.show(Image.HAPPY)
