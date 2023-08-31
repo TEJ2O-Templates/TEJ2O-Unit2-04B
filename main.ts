1bdd7404c692846e48ea882aca1617011f54059e
Patrick Coxall <mr.coxall@mths.ca>
2023-08-31 13:56:48 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -2,7 +2,22 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program calculates area and perimeter of rectangle.
 */
 
-basic.showString('Hello, World!')
+// variables
+let area = 3 * 5
+let perimeter = 2 * (3 + 5)
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+  basic.clearScreen()
+  basic.showString('The dimensions of a rectangle are 5 cm and 3 cm.')
+  basic.pause(1000)
+  basic.showString("The area is: " + area + " cm^2.")
+  basic.pause(1000)
+  basic.showString("The perimeter is: " + perimeter + " cm.")
+  basic.clearScreen()
+})
